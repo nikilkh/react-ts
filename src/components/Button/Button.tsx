b121585cd0d212b965a4953878233612bbe62cfe
nikilkh <nikilkh241295@gmail.com>
2022-08-23 10:10:38 +0530

    NH | Extracted interfaces to a new file called types  and imported from there

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { ButtonStyle } from "./Button.styled";
-interface props {
-  children: React.ReactNode;
-  onclick: React.MouseEventHandler<HTMLButtonElement>;
-}
+import { props } from "./Button.types";
+
 export const Button = (props: props) => {
   return <ButtonStyle onClick={props.onclick}>{props.children}</ButtonStyle>;
 };
